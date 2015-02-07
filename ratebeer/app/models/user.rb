fdb8eab9d13abe273b18d2bd1a3a721273609c1a
Saknet <aandra@localhost.localdomain>
2015-02-07 23:41:19 +0200

    travis

--- ratebeer/app/models/user.rb
+++ ratebeer/app/models/user.rb
@@ -14,4 +14,13 @@ class User < ActiveRecord::Base
   has_many :memberships, dependent: :destroy
   has_many :beer_clubs, through: :memberships, dependent: :destroy
 
+  def favorite_beer
+    return nil if ratings.empty?
+    ratings.order(score: :desc).limit(1).first.beer
+  end
+
+  def favorite_style
+    return nil if ratings.empty?
+  end
+
 end
